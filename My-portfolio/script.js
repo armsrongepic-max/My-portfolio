53b63e8a6763e54bd3212687f3ebe54d0dc33c4d
armsrongepic-max <armsrongepic@gmail.com>
2026-03-05 14:46:32 +0100

    Add files via upload

---
+++ My-portfolio/script.js
@@ -0,0 +1,25 @@
+// Welcome Message
+alert("Welcome to Strong Epic's Portfolio Website!");
+
+// Change Header Color When Clicked
+const header = document.querySelector("header h1");
+
+header.addEventListener("click", function () {
+    header.style.color = "red";
+});
+
+// Contact Form Validation Message
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Stops page refresh
+
+    alert("Thank you for contacting me! I will reply soon.");
+
+    form.reset(); // Clears form after submit
+});
+const darkBtn = document.getElementById("darkModeBtn");
+
+darkBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
